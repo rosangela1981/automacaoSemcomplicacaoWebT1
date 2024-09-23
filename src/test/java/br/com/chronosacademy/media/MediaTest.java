ddc7e567307d5bceb98e0922df6246ed469a907a
rosangela1920 <rosangelanascimentotecnol@hotmail.com>
2024-09-22 23:05:11 -0300

    aula 05 06 07 selenium anotacoes e seletores

---
+++ src/test/java/br/com/chronosacademy/media/MediaTest.java
@@ -0,0 +1,32 @@
+package br.com.chronosacademy.media;
+
+
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MediaTest {
+    //Aprovado
+    //Reprovado
+
+  @Test
+    public void validaAprovado(){
+        Media media= new Media();
+        String resultado = media.calculaMedia(5.0,5.0);
+         assertEquals("Aprovado", resultado);
+
+    }
+
+    @Test
+    public void validaReprovado() {
+        Media media = new Media();
+        String resultado = media.calculaMedia(4.9, 5.0);
+        assertEquals("Reprovado",resultado);
+
+
+    }
+
+}
+
